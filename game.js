b929c612d3eb982cc4e6e9848b3522ccd81c64fb
ray851107 <ray851107@gmail.com>
2016-03-30 20:01:41 +0800

    air jump

--- game.js
+++ game.js
@@ -6,15 +6,20 @@ var game = new Phaser.Game('100', '100', Phaser.AUTO, 'game', {
     update: update
 }, true);
 
-
-var poi;
-
 function preload() {
     game.load.image('poi', 'poi.gif');
     game.load.image('snail', 'snail.gif');
 }
 
+var cursors;
+
+var poi;
+var rotation;
+
 function create() {
+    cursors = game.input.keyboard.createCursorKeys();
+    cursors.up.onDown.add(jump);
+
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     poi = game.add.sprite(0, 0, 'poi');
@@ -26,15 +31,12 @@ function create() {
 
     poi.anchor.setTo(0.5, 0.5);
     poi.rotation = Math.PI * 0.1;
-    game.add.tween(poi)
+    rotation = game.add.tween(poi)
         .to({ rotation: Math.PI * -0.1 }, 500, Phaser.Easing.Quadratic.InOut, true, 0, -1, true);
-
 }
 
-var cursors;
-
+var jumpCount;
 function update() {
-    cursors = cursors || game.input.keyboard.createCursorKeys();
     if (!cursors.left.isDown && !cursors.right.isDown) {
         poi.body.velocity.x = 0;    
     }
@@ -44,8 +46,29 @@ function update() {
     if (cursors.right.isDown) {
         poi.body.velocity.x = 200;
     }
-    if (cursors.up.isDown &&
-        poi.body.y == game.world.height - poi.body.height) {
-        poi.body.velocity.y = -1000;
+
+    /* 在地上 */
+    if (poi.body.y == game.world.height - poi.body.height) {
+        jumpCount = 0;
+        poi.body.angularVelocity = 0;
+        rotation.resume();
     }
+}
+
+function jump() {
+    var MAX_JUMP = 2;
+
+    if (jumpCount >= MAX_JUMP) return;
+
+    poi.body.velocity.y = -1000;
+    
+    /* 多段跳 */
+    if (jumpCount !== 0) {
+        rotation.pause();
+        poi.body.angularVelocity = 10000;
+        poi.body.velocity.y = -1000;
+    
+    }
+
+    ++jumpCount;
 }
